c1ad48931520251a56ede1414c1a045467a4ff16
cof <philaimmamah@yahoo.com>
2023-05-09 16:17:38 +0200

    switch age more or less than 18

---
+++ exo5.php
@@ -0,0 +1,72 @@
+<?php
+$age = 20;
+switch($age){
+    
+    case 0:
+        echo" $age est plus petit que 18";
+        break;
+    case 1:
+            echo" $age est plus petit que 18";
+            break;
+    case 2:
+            echo" $age est plus petit que 18";
+                break;    
+    case 3:
+            echo" $age est plus petit que 18";
+                    break; 
+    case 4:
+            echo" $age est plus petit que 18";
+                    break;
+    case 5:
+        echo" $age est plus petit que 18";
+        break;
+    case 6:
+            echo" $age est plus petit que 18";
+            break;
+    case 7:
+            echo" $age est plus petit que 18";
+                break;    
+    case 8:
+            echo" $age est plus petit que 18";
+                    break; 
+    case 9:
+            echo" $age est plus petit que 18";
+                    break; 
+    case 10:
+        echo" $age est plus petit que 18";
+        break;
+    case 11:
+            echo" $age est plus petit que 18";
+            break;
+    case 12:
+            echo" $age est plus petit que 18";
+                break;    
+    case 13:
+            echo" $age est plus petit que 18";
+                    break; 
+    case 14:
+            echo" $age est plus petit que 18";
+                    break;
+    case 15:
+        echo" $age est plus petit que 18";
+        break;
+    case 16:
+            echo" $age est plus petit que 18";
+            break;
+    case 17:
+            echo" $age est plus petit que 18";
+                break;    
+    case 18:
+            echo" $age est égale à 18";
+                    break; 
+    case 19:
+            echo" $age est plus grand que 18";
+                    break;
+    case 20:
+        echo "$age est plus grand que 18";
+        break;
+    case -1:
+        echo"$age y a un probleme";
+        break;
+}
+?>
