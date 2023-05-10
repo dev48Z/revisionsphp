00d119121770e6398bacd0357e0dbb0fdd1527f2
cof <philaimmamah@yahoo.com>
2023-05-10 11:17:18 +0200

    switch age 18 or not

--- exo5.php
+++ exo5.php
@@ -1,70 +1,16 @@
 <?php
 $age = 20;
 switch($age){
-    
-    case 0:
-        echo"  je suis mineur";
-        break;
-    case 1:
-            echo"  je suis mineur";
-            break;
-    case 2:
-            echo"  je suis mineur";
-                break;    
-    case 3:
-            echo"  je suis mineur";
-                    break; 
-    case 4:
-            echo"  je suis mineur";
-                    break;
-    case 5:
-        echo"  je suis mineur";
-        break;
-    case 6:
-            echo"  je suis mineur";
-            break;
-    case 7:
-            echo"  je suis mineur";
-                break;    
-    case 8:
-            echo"  je suis mineur";
-                    break; 
-    case 9:
-            echo"  je suis mineur";
-                    break; 
-    case 10:
-        echo"  je suis mineur";
-        break;
-    case 11:
-            echo"  je suis mineur";
-            break;
-    case 12:
-            echo"  je suis mineur";
-                break;    
-    case 13:
-            echo"  je suis mineur";
-                    break; 
-    case 14:
-            echo"  je suis mineur";
-                    break;
-    case 15:
-        echo"  je suis mineur";
-        break;
-    case 16:
-            echo"  je suis mineur";
-            break;
-    case 17:
-            echo"  je suis mineur";
-                break;    
-    case 18:
-            echo"  je suis majeur";
-                    break; 
-    case 19:
-            echo"je suis majeur";
-                    break;
-    case 20:
+    case $age >= 18:
         echo "je suis majeur";
         break;
+    case $age <= 18 & $age > 0:
+        echo "je suis mineur";
+        break;
+    case $age < 0:
+        echo "erreur age impossible";
     
 }
 ?>
+
+
